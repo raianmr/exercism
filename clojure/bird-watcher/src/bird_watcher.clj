dce41a8dc402ce420f57d299e5752ec758403a4a
Mahmudur Rahman <raianmr@gmail.com>
2022-11-26 20:53:33 +0600

    enough s-expressions for one day

---
+++ clojure/bird-watcher/src/bird_watcher.clj
@@ -0,0 +1,20 @@
+(ns bird-watcher)
+
+(def last-week [0 2 5 3 7 8 4])
+
+(defn today [birds] (last birds))
+
+(defn inc-bird [birds]
+  (update birds (dec (count birds)) inc))
+
+(defn day-without-birds? [birds]
+  (not (every? pos? birds)))
+
+(defn n-days-count [birds n]
+  (reduce + (take n birds)))
+
+(defn busy-days [birds]
+  (count (filter (fn [days] (>= days 5)) birds)))
+
+(defn odd-week? [birds]
+  (= birds [1 0 1 0 1 0 1]))
